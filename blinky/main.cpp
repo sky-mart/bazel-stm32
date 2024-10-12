42595dcbe250a21362783d2bae56e7f90bb61b84
Vlad <sky-mart@hotmail.com>
2024-10-12 13:34:19 +0200

    Add and use HAL

--- blinky/main.cpp
+++ blinky/main.cpp
@@ -1,29 +1,41 @@
 #include "stm32f3xx.h"
+#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal_gpio.h"
 
-GPIO_TypeDef * const port = GPIOE;
-const int led = 13;
+GPIO_TypeDef * const led_port = GPIOE;
+const auto led_pin = GPIO_PIN_8;
 
-void softDelay(volatile uint32_t time)
+void delay_ms(uint32_t ms)
 {
-	while (time > 0) {
-		time--;
-	}
+  HAL_Delay(ms / uwTickFreq);
 }
 
 int main()
 {
-    RCC->AHBENR |= RCC_AHBENR_GPIOEEN; // Clock
+  HAL_Init();
 
-	port->MODER 	|= 1 << led*2; 	// General purpose output mode
-	port->OTYPER 	&= ~(1 << led); // Push-pull
-	port->OSPEEDR 	|= 3 << led*2;	// High speed
-	port->PUPDR 	|= 1 << led*2;	// Pull-up
+  RCC->AHBENR |= RCC_AHBENR_GPIOEEN; // Clock
 
-	while (true)
-    {
-		port->BSRR |= 1 << led;			// Output "1"
-		softDelay(400000);
-		port->BSRR |= 1 << (led + 16);	// Output "0"
-		softDelay(400000);
-	}
+  GPIO_InitTypeDef led_config;
+  led_config.Pin = led_pin;
+  led_config.Mode = GPIO_MODE_OUTPUT_PP;
+  led_config.Pull = GPIO_PULLDOWN;
+  led_config.Speed = GPIO_SPEED_FREQ_LOW;
+
+  HAL_GPIO_Init(led_port, &led_config);
+
+  while (true)
+  {
+    HAL_GPIO_WritePin(led_port, led_pin, GPIO_PIN_SET);
+    delay_ms(500U);
+    HAL_GPIO_WritePin(led_port, led_pin, GPIO_PIN_RESET);
+    delay_ms(500U);
+  }
+}
+
+extern "C" {
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
 }
--- third_party/stm32/defs.bzl
+++ third_party/stm32/defs.bzl
@@ -26,7 +26,7 @@ def _debug_stm32_impl(ctx):
     script_content = """#!/bin/sh
 openocd -f board/stm32f3discovery.cfg &
 trap "kill %1" EXIT # kill openocd on exit
-arm-none-eabi-gdb -i=mi -q -ex "target remote :3333" -ex "set substitute-path /proc/self/cwd $BUILD_WORKSPACE_DIRECTORY" {elf_to_debug}
+arm-none-eabi-gdb -q -ex "target remote :3333" -ex "set substitute-path /proc/self/cwd $BUILD_WORKSPACE_DIRECTORY" {elf_to_debug}
 """.format(elf_to_debug=elf.short_path)
 
     script = ctx.actions.declare_file("%s.sh" % ctx.label.name)
