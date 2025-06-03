89ec09b8369cdbed9f1ef2c034557a4812896503
Vlad <sky-mart@hotmail.com>
2025-06-03 21:26:36 +0200

    Use jinja2 to generate a header

--- mcu/geninit.py
+++ mcu/geninit.py
@@ -1,5 +1,20 @@
+from jinja2 import Environment, FileSystemLoader
+from dataclasses import dataclass
+
+@dataclass
+class Peripheral:
+    alias: str
+    variant: str
+
+
 def main():
-    print("Hello, World!")
+    env = Environment(loader=FileSystemLoader("mcu"))
+    template = env.get_template("board.h.j2")
+    output = template.render(mcu_header = "stm32f303xc.h", peripherals=[
+        Peripheral("CONSOLE_USART", "USART2"),
+        Peripheral("GYRO_SPI", "SPI1"),
+    ])
+    print(output)
 
 if __name__ == '__main__':
     main()
